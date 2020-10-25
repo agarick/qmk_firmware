b50319b6a5e3ab74a898e6f4cbd242cfb82fe974
blobject <agaric@protonmail.com>
2020-10-25 13:17:05 +0100

    change namespace

--- keyboards/planck/keymaps/blobject/keymap.c
+++ keyboards/planck/keymaps/blobject/keymap.c
@@ -72,13 +72,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
      * |----+----+----+----+----+----+----+----+----+----+----+----|
      * |////| f1 | f2 | f3 | f4 | f5 | f6 | f7 | f8 | f9 | f10|////|
      * |----+----+----+----+----+----+----+----+----+----+----+----|
-     * |    |    |////|////|////|/////////|XXXX|////|////| f11| f12|
+     * | f11| f12|////|////|////|/////////|XXXX|////|////|    |    |
      * '-----------------------------------------------------------'
      */
-    _____,KC_1   ,KC_2 ,KC_3   ,KC_4  ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,_____,
-    _____,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_PIPE,KC_BSLS,_____,
-    _____,KC_F1  ,KC_F2,KC_F3  ,KC_F4 ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,_____,
-    __x__,__x__  ,_____,_____  ,_____ ,_____  ,_____  ,_____  ,_____  ,_____  ,KC_F11 ,KC_F12
+    _____ ,KC_1   ,KC_2 ,KC_3   ,KC_4  ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,_____,
+    _____ ,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_PIPE,KC_BSLS,_____,
+    _____ ,KC_F1  ,KC_F2,KC_F3  ,KC_F4 ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,_____,
+    KC_F11,KC_F12 ,_____,_____  ,_____ ,_____  ,_____  ,_____  ,_____  ,_____  ,__x__  ,__x__
   ),
 
   [2] = LAYOUT_planck_grid(
